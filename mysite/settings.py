08359a28752488911cbf63f878de47969ea257a5
lbrlarissa <51515936+lbrlarissa@users.noreply.github.com>
2019-06-12 18:54:06 -0300

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b5e_l9*vmzsybbk7s!#%k#wijzmdb2(xtbpl9ez17kq%&wb@m1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
